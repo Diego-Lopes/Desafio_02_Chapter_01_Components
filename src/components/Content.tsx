79718ef060217fa1da023b3eab04cdf9f29b9ab6
Diego Lopes <43974604+Diego-Lopes@users.noreply.github.com>
2021-11-30 18:59:39 -0300

    ref: separating into components

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,33 @@
+import { MovieCard } from "./MovieCard";
+import { useMovies } from "./MovieContext";
+import "../styles/content.scss";
 export function Content() {
-  // Complete aqui
+  const { selectedGenre, movies } = useMovies();
+  return (
+    <>
+      <div style={{ display: "flex", paddingLeft: "150px" }}>
+        <div className="container">
+          <header>
+            <span className="category">
+              Categoria:<span> {selectedGenre.title}</span>
+            </span>
+          </header>
+
+          <main>
+            <div className="movies-list">
+              {movies.map((movie) => (
+                <MovieCard
+                  key={movie.imdbID}
+                  title={movie.Title}
+                  poster={movie.Poster}
+                  runtime={movie.Runtime}
+                  rating={movie.Ratings[0].Value}
+                />
+              ))}
+            </div>
+          </main>
+        </div>
+      </div>
+    </>
+  );
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,31 @@
+import { useMovies } from "./MovieContext";
+import { Button } from "./Button";
+import "../styles/sidebar.scss";
 export function SideBar() {
-  // Complete aqui
+  const { genres, handleClickButton, selectedGenreId } = useMovies();
+  console.log(selectedGenreId);
+
+  return (
+    <>
+      <div style={{ display: "flex" }}>
+        <nav className="sidebar">
+          <span>
+            Watch<p>Me</p> 💻
+          </span>
+
+          <div className="buttons-container">
+            {genres.map((genre) => (
+              <Button
+                key={String(genre.id)}
+                title={genre.title}
+                iconName={genre.name}
+                onClick={() => handleClickButton(genre.id)}
+                selected={selectedGenreId === genre.id}
+              />
+            ))}
+          </div>
+        </nav>
+      </div>
+    </>
+  );
 }
